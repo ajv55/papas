3463e270b931ef2cc558408148dd48b82ce2a71f
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-08-04 21:23:53 -0400

    created the privacy page

--- src/app/[locale]/privacy/page.tsx
+++ src/app/[locale]/privacy/page.tsx
@@ -6,70 +6,65 @@ import Footer from '@/app/components/mainPage/footer';
 const Page = () => (
   <div className={`${style.background} w-full mx-auto`}>
     <Header />
-    <div className='container mx-auto'>
-      <h1 className="lg:text-6xl text-3xl mt-4 font-bold mb-6 text-center" style={{ color: '#FB8B24' }}>Terms of Service</h1>
-      <p className="text-gray-600 mb-8 text-center">Last Updated: July, 17, 2024</p>
-      
-      <section className="mb-8">
-        <h2 className="lg:text-3xl text-xl font-semibold mb-4" style={{ color: '#E36414' }}>Welcome to Papas Llenos!</h2>
-        <p className="text-gray-700">These Terms of Service (&quot;Terms&quot;) govern your use of our website and services. By accessing or using our website, you agree to comply with and be bound by these Terms.</p>
-      </section>
+    <div className="max-w-6xl mx-auto">
+        <h1 className="lg:text-6xl text-3xl font-bold mt-6 mb-6 text-primary-dark">Privacy Policy</h1>
+        <p className="text-gray-600 mb-6">Last Updated: July 17, 2024</p>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Acceptance of Terms</h2>
-        <p className="text-gray-700">By accessing our website, you agree to comply with these Terms. If you do not agree with any part of these Terms, you must not use our website.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Introduction</h2>
+          <p className="text-gray-700">Papas Llenos (&quot;we,&quot; &quot;our,&quot; or &quot;us&quot;) is committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website.</p>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Changes to Terms</h2>
-        <p className="text-gray-700">We reserve the right to modify these Terms at any time. Any changes will be effective immediately upon posting. Your continued use of the website after any modifications indicate your acceptance of the new Terms.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Information We Collect</h2>
+          <p className="text-gray-700">We may collect information about you in a variety of ways. The information we may collect on the website includes:</p>
+          <ul className="list-disc list-inside text-gray-700 mt-4">
+            <li><strong>Personal Data:</strong> Personally identifiable information, such as your name, shipping address, email address, and telephone number, that you voluntarily give to us when you register with the website or place an order.</li>
+            <li><strong>Derivative Data:</strong> Information our servers automatically collect when you access the website, such as your IP address, browser type, operating system, access times, and the pages you have viewed directly before and after accessing the website.</li>
+          </ul>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Use of Our Website</h2>
-        <p className="text-gray-700">You agree to use our website only for lawful purposes and in accordance with these Terms. You must not use our website:</p>
-        <ul className="list-disc list-inside text-gray-700">
-          <li>In any way that violates any applicable local, national, or international law or regulation.</li>
-          <li>To transmit, or procure the sending of, any unsolicited or unauthorized advertising or promotional material.</li>
-          <li>To impersonate or attempt to impersonate Papas Llenos, a Papas Llenos employee, another user, or any other person or entity.</li>
-        </ul>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Use of Your Information</h2>
+          <p className="text-gray-700">Having accurate information about you permits us to provide you with a smooth, efficient, and customized experience. Specifically, we may use the information collected to:</p>
+          <ul className="list-disc list-inside text-gray-700 mt-4">
+            <li>Process your transactions.</li>
+            <li>Send you administrative information, such as order confirmations and updates.</li>
+            <li>Improve our website and customer service.</li>
+            <li>Respond to your inquiries and offer customer support.</li>
+            <li>Send you marketing communications.</li>
+          </ul>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Intellectual Property Rights</h2>
-        <p className="text-gray-700">All content on this website, including text, graphics, logos, images, and software, is the property of Papas Llenos and is protected by intellectual property laws. You may not reproduce, distribute, or create derivative works from any content on our website without our express written permission.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Disclosure of Your Information</h2>
+          <p className="text-gray-700">We may share information we have collected about you in certain situations. Your information may be disclosed as follows:</p>
+          <ul className="list-disc list-inside text-gray-700 mt-4">
+            <li><strong>By Law or to Protect Rights:</strong> If we believe the release of information about you is necessary to respond to legal process, to investigate or remedy potential violations of our policies, or to protect the rights, property, and safety of others, we may share your information as permitted or required by any applicable law, rule, or regulation.</li>
+            <li><strong>Third-Party Service Providers:</strong> We may share your information with third parties that perform services for us or on our behalf, such as payment processing, data analysis, email delivery, hosting services, customer service, and marketing assistance.</li>
+          </ul>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>User Accounts</h2>
-        <p className="text-gray-700">To access certain features of our website, you may be required to create an account. You agree to provide accurate, current, and complete information during the registration process and to update such information to keep it accurate, current, and complete.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Security of Your Information</h2>
+          <p className="text-gray-700">We use administrative, technical, and physical security measures to help protect your personal information. While we have taken reasonable steps to secure the personal information you provide to us, please be aware that despite our efforts, no security measures are perfect or impenetrable.</p>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Order Management</h2>
-        <p className="text-gray-700">Our website allows you to place orders for products. By placing an order, you agree to pay the specified amount for the products ordered and any applicable taxes and shipping fees.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Policy for Children</h2>
+          <p className="text-gray-700">We do not knowingly solicit information from or market to children under the age of 13. If we learn that we have collected personal information from a child under age 13 without verification of parental consent, we will delete that information as quickly as possible.</p>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Disclaimer of Warranties</h2>
-        <p className="text-gray-700">Our website is provided &quot;as is&quot; and &quot;as available&quot; without any warranties of any kind, either express or implied. We do not warrant that our website will be uninterrupted, error-free, or free of viruses or other harmful components.</p>
-      </section>
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Changes to This Privacy Policy</h2>
+          <p className="text-gray-700">We may update this Privacy Policy from time to time. We will notify you of any changes by updating the &quot;Last Updated&quot; date of this Privacy Policy. You are encouraged to review this Privacy Policy periodically to stay informed of updates.</p>
+        </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Limitation of Liability</h2>
-        <p className="text-gray-700">In no event will Papas Llenos, its affiliates, or their licensors, service providers, employees, agents, officers, or directors be liable for damages of any kind arising out of or in connection with your use of our website.</p>
-      </section>
-
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Governing Law</h2>
-        <p className="text-gray-700">These Terms are governed by and construed in accordance with the laws of [Your Country/State], without regard to its conflict of law principles.</p>
-      </section>
-
-      <section className="mb-10">
-        <h2 className="text-2xl font-semibold mb-4" style={{ color: '#E36414' }}>Contact Us</h2>
-        <p className="text-gray-700">If you have any questions about these Terms, please contact us at <a href="mailto:contact@papasllenos.com" className="text-indigo-600 underline">contact@papasllenos.com</a>.</p>
-      </section>
-    </div>
+        <section className='mb-10'>
+          <h2 className="text-2xl font-semibold mb-4 text-primary-dark">Contact Us</h2>
+          <p className="text-gray-700">If you have questions or comments about this Privacy Policy, please contact us at <a href="mailto:contact@papasllenos.com" className="text-primary-light hover:underline">contact@papasllenos.com</a>.</p>
+        </section>
+      </div>
     <Footer />
   </div>
 );
